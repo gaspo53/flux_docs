0a7bdda02b436c4073ffb5a7ee3b1d83ff80d598
root <root@gaspar-laptop.(none)>
2013-02-07 23:25:22 -0300

    Finishing up...

---
+++ apps/backend/modules/sfGuardAuth/templates/signinSuccess.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+    <?php include_title() ?>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <?php include_stylesheets() ?>
+    <?php include_javascripts() ?>
+    
+    <?php use_helper('I18N') ?>
+    <style type="text/css">
+      body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+      }
+
+      .form-signin {
+        max-width: 300px;
+        padding: 19px 29px 29px;
+        margin: 0 auto 20px;
+        background-color: #fff;
+        border: 1px solid #e5e5e5;
+        -webkit-border-radius: 5px;
+           -moz-border-radius: 5px;
+                border-radius: 5px;
+        -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+           -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                box-shadow: 0 1px 2px rgba(0,0,0,.05);
+      }
+      .form-signin .form-signin-heading,
+      .form-signin .checkbox {
+        margin-bottom: 10px;
+      }
+      .form-signin input[type="text"],
+      .form-signin input[type="password"] {
+        font-size: 16px;
+        height: auto;
+        margin-bottom: 15px;
+        padding: 7px 9px;
+      }
+
+    </style>
+  </head>
+  <body>
+
+      <div class="container">
+
+        <form class="form-signin" action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
+          <h2 class="form-signin-heading"><?php echo __('Please sign in') ?></h2>
+          <?php echo $form ?>          
+          <button class="btn btn-large btn-primary" type="submit">Sign in</button>
+        </form>
+
+      </div> <!-- /container -->
+  </body>
+</html>
